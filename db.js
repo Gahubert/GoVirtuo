62308583bb35727e9ddc03500168d8b4c7494d0b
gabriel huebert <ghubert@student.42.fr>
2019-05-24 13:09:09 +0200

    [UPD] cleaning up a bit db.js file

--- db.js
+++ db.js
@@ -4,12 +4,4 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://root:" + config.db_pass + "@axileo-d31gv.mongodb.net/test?retryWrites=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
-/*client.connect(err => {
-  const collection = client.db("GoVirtuo").collection("stations");
-  var cursor = collection.find();
-  cursor.each(function (err, doc) {
-    console.log(doc);
-  });
-});*/
-
 module.exports = client;
